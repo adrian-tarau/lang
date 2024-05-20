70ff6d7a0335fb242982ca7e384ccc76a1918a0b
alex-tarau <alex@tarau.net>
2024-05-20 12:51:33 -0400

    Unit testing the ClassUtils

---
+++ src/test/java/net/microfalx/lang/ClassUtilsTest.java
@@ -0,0 +1,130 @@
+package net.microfalx.lang;
+
+import org.apache.commons.lang3.math.Fraction;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassUtilsTest {
+
+    @Test
+    void isBaseClassWithObject() {
+        assertTrue(ClassUtils.isBaseClass(Integer.valueOf("80")));
+    }
+
+    @Test
+    void isBaseClassWithClass() {
+        assertTrue(ClassUtils.isBaseClass(Boolean.class));
+    }
+
+    @Test
+    void getNameWithClass() {
+        assertEquals("java.lang.Number", ClassUtils.getName(Number.class));
+        assertEquals("", ClassUtils.getName(null));
+    }
+
+    @Test
+    void getNameWithClassAndDefaultValue() {
+        assertEquals("java.lang.Number", ClassUtils.getName(Number.class, "The class is null"));
+        assertEquals("The class is null", ClassUtils.getName(null, "The class is null"));
+    }
+
+    @Test
+    void getNameWithObject() {
+        assertEquals("java.lang.Integer", ClassUtils.getName(Integer.valueOf("80")));
+        assertEquals("", ClassUtils.getName(null));
+    }
+
+    @Test
+    void getNameWithObjectAndDefaultValue() {
+        assertEquals("java.lang.Integer", ClassUtils.getName(Integer.valueOf("80"), "The class is null"));
+        assertEquals("The class is null", ClassUtils.getName(null, "The class is null"));
+    }
+
+
+    @Test
+    void getSimpleName() {
+        assertEquals("java.lang.Integer", ClassUtils.getSimpleName(Integer.valueOf("80")));
+        assertEquals("", ClassUtils.getSimpleName((Object) null));
+
+    }
+
+    @Test
+    void getSimpleNameWithDefaultValue() {
+        assertEquals("java.lang.Integer", ClassUtils.getSimpleName(Integer.valueOf("80"), "The class is null"));
+        assertEquals("The class is null", ClassUtils.getSimpleName((Object) null, "The class is null"));
+    }
+
+    @Test
+    void getSimpleNameWithString() {
+        assertEquals("I am writing java code", ClassUtils.getSimpleName("I am writing java code"));
+        assertEquals("",ClassUtils.getSimpleName((Object) null));
+    }
+
+
+    @Test
+    void isSubClassOfWithObject() {
+        assertFalse(ClassUtils.isSubClassOf((Object) null, Number.class));
+        assertTrue(ClassUtils.isSubClassOf(Integer.valueOf("80"), Number.class));
+    }
+
+    @Test
+    void isSubClassOfWithClass() {
+        assertTrue(ClassUtils.isSubClassOf(Integer.class, Number.class));
+    }
+
+    @Test
+    void isJdkClassWithObject() {
+        assertTrue(ClassUtils.isJdkClass(Integer.valueOf("80")));
+    }
+
+    @Test
+    void testIsJdkClassWithClass() {
+        assertTrue(ClassUtils.isJdkClass(Integer.class));
+    }
+
+    @Test
+    void canInstantiate() {
+        assertFalse(ClassUtils.canInstantiate(Number.class));
+        assertFalse(ClassUtils.canInstantiate(null));
+    }
+
+    @Test
+    void resolveProviders() {
+        assertIterableEquals(Collections.EMPTY_LIST, ClassUtils.resolveProviders(Number.class));
+    }
+
+    @Test
+    void resolveProviderInstances() {
+        assertIterableEquals(Collections.EMPTY_LIST, ClassUtils.resolveProviderInstances(Number.class));
+    }
+
+    @Test
+    void clearCaches() {
+    }
+
+    @Test
+    void getClassParametrizedTypes() {
+        assertIterableEquals(Collections.EMPTY_LIST, ClassUtils.getClassParametrizedTypes(Fraction.class));
+    }
+
+    @Test
+    void getClassParametrizedTypeWithIndex() {
+        assertEquals(null, ClassUtils.getClassParametrizedType(Fraction.class,0));
+
+    }
+
+    @Test
+    void getClassParametrizedTypeWithClass() {
+        assertEquals(null, ClassUtils.getClassParametrizedType(Fraction.class, Number.class));
+
+    }
+
+    @Test
+    void create() {
+        assertEquals("",ClassUtils.create(String.class));
+        assertThrows(Exception.class,() -> ClassUtils.create(Number.class));
+    }
+}
