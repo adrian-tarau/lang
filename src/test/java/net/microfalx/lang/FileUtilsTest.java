6001a129be4bf9b46802b7423001ef4cb3cb5437
alex-tarau <alex@tarau.net>
2024-05-22 10:19:53 -0400

    Unit testing the FileUtils

---
+++ src/test/java/net/microfalx/lang/FileUtilsTest.java
@@ -0,0 +1,59 @@
+package net.microfalx.lang;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class FileUtilsTest {
+
+    private static File file;
+
+    @BeforeAll
+    static void createFile() throws IOException {
+        file = new File("target/files/test.txt");
+    }
+
+    @Test
+    void getFileExtension() {
+        assertNull(FileUtils.getFileExtension(null));
+        assertEquals("txt", FileUtils.getFileExtension(file.getName()));
+    }
+
+    @Test
+    void getFileName() throws IOException {
+        assertNull(FileUtils.getFileName(null));
+        assertEquals("test.txt",FileUtils.getFileName(file.getPath()));
+    }
+
+    @Test
+    void getParentPath() {
+        assertEquals("target\\files",FileUtils.getParentPath(file.getPath()));
+
+    }
+
+    @Test
+    void getContentType() {
+        assertEquals("text/plain",FileUtils.getContentType(file.getName()));
+    }
+
+    @Test
+    void validateDirectoryExists() {
+        assertEquals(file.getParentFile(),FileUtils.validateDirectoryExists(file.getParentFile()));
+    }
+
+    @Test
+    void validateFileExists() {
+        assertEquals(new File("target\\files\\test.txt"),FileUtils.validateFileExists(file));
+    }
+
+    @AfterAll
+    static void deleteFile() {
+        file.deleteOnExit();
+    }
+}
